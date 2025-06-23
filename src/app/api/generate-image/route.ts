0f0f4b156194b86654bf1c5f181bb70579584f8d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-06-24 07:39:06 +0900

    画像生成APIのリクエスト処理を追加し、axiosとform-dataを使用して画像を生成し、ファイルとして保存する機能を実装しました。また、package.jsonとpackage-lock.jsonにaxiosとform-dataの依存関係を追加しました。

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,8 +1,32 @@
 import { NextResponse } from "next/server";
-
+import fs from "node:fs";
+import axios from "axios";
+import FormData from "form-data";
 export default function POST(req: Request) {
   try {
-    // リクエストの処理をここに記述
+    const payload = {
+      prompt: "Lighthouse on a cliff overlooking the ocean",
+      output_format: "webp",
+    };
+
+    const response = await axios.postForm(
+      `https://api.stability.ai/v2beta/stable-image/generate/core`,
+      axios.toFormData(payload, new FormData()),
+      {
+        validateStatus: undefined,
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: `Bearer sk-MYAPIKEY`,
+          Accept: "image/*",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      fs.writeFileSync("./lighthouse.webp", Buffer.from(response.data));
+    } else {
+      throw new Error(`${response.status}: ${response.data.toString()}`);
+    }
   } catch (error) {
     console.error("エラーが発生しました:", error);
     return NextResponse.json(
