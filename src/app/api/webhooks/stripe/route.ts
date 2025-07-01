eed6397171ce4dee5dbcde72b443a1b2d8d06764
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-02 07:16:07 +0900

    Stripeのチェックアウトセッション完了時にサブスクリプション情報をデータベースに登録する機能を追加し、サブスクリプションステータスの管理を強化しました。また、エラーハンドリングを実装し、未処理のイベントタイプに対するログ出力を追加しました。

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,4 +1,6 @@
 import { stripe } from "@/config/stripe";
+import { prisma } from "@/lib/prisma";
+import { SubscriptionStatus } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -21,17 +23,55 @@ export async function POST(request: NextRequest) {
   }
 
   switch (event.type) {
-    case "payment_intent.succeeded":
-      const paymentIntent = event.data.object;
-      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+    case "checkout.session.completed":
+      const session = event.data.object;
+      // エラーハンドリング
+      if (!session.metadata || !session.subscription) {
+        return new NextResponse("Session Error", { status: 500 });
+      }
+      const subscription = (await stripe.subscriptions.retrieve(
+        session.subscription as string
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      )) as any;
+
+      let subscriptionStatus: SubscriptionStatus = "FREE";
+
+      switch (subscription.items.data[0].price.id) {
+        case "price_1Rfg81CjQk4bcqCwb0PUwQCO":
+          subscriptionStatus = "FREE";
+          break;
+        case "price_1Rfg8OCjQk4bcqCwZOFSmzcR":
+          subscriptionStatus = "BASIC";
+          break;
+        case "price_1Rfg8dCjQk4bcqCws1DnkJMU":
+          subscriptionStatus = "PRO";
+          break;
+      }
+      // 初回はサブスク情報の登録のみ
+      await prisma.user.update({
+        where: {
+          clerkId: session.metadata.clerkId,
+        },
+        data: {
+          subscriptionStatus: subscriptionStatus,
+          subscription: {
+            create: {
+              stripeSubscriptionID: subscription.id,
+              stripePriceID: subscription.items.data[0].price.id,
+              stripeCurrentPeriodEnd: new Date(
+                subscription.current_period_end * 1000
+              ),
+            },
+          },
+        },
+      });
       break;
     case "payment_method.attached":
-      const paymentMethod = event.data.object;
-
+      // Payment method attached - implement logic as needed
       break;
     default:
       console.log(`Unhandled event type ${event.type}.`);
   }
 
-  response.send();
+  return new NextResponse("Success", { status: 200 });
 }
